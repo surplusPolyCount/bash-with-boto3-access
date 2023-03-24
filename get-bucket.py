a97f1bd669b1dd7339305cbfbb8fe169784c5cba
otter <simonhrosenthal@gmail.com>
2023-03-24 14:45:08 -0700

    updated to something more vanilla

--- get-bucket.py
+++ get-bucket.py
@@ -3,6 +3,17 @@ import sys
 
 def main():
         print(f"attempting access bucket {sys.argv[1]}")
+
+        s3 = boto3.resource("s3")
+        bucket = 'my-bucket'
+        #Make sure you provide / in the end
+        prefix = 'hospital17/'  
+
+        client = boto3.client('s3')
+        result = client.list_objects(Bucket=bucket, Prefix=prefix, Delimiter='/')
+        for o in result.get('CommonPrefixes'):
+                print 'sub folder : ', o.get('Prefix')
+        """
         s3_client = boto3.client(
                 service_name = 's3',
                 endpoint_url = sys.argv[1]
@@ -14,6 +25,7 @@ def main():
                         print(f"   {bucket['Name']}")
         except:
                 print("could not find any buckets")
+        """
 
 if __name__ == '__main__':
         main()
